fc481dbba99f6e5a5252202e63575fd501a0b388
Welinton dos Santos Lage <WelintonLage.Dev@hotmail.com>
2025-05-27 20:58:12 -0300

    aula 27/05/2025

---
+++ Curso/aula13/pop-push-shift-unShift.js
@@ -0,0 +1,12 @@
+const nomes = [];
+
+nomes.push("Adriana");
+nomes.push("Ricardo");
+nomes.push("José");
+nomes.pop();
+nomes.unshift("Leonardo");
+nomes.unshift("Welinton");
+nomes.unshift("Roberto");
+nomes.shift();
+
+console.log(nomes);
