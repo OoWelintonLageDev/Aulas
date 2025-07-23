50e0c8f146aa86ceafa789725e7472a0c8cdf5d0
Welinton dos Santos Lage <WelintonLage.Dev@hotmail.com>
2025-07-22 21:02:23 -0300

    Modificação do nome da pasta

---
+++ Curso/aula20-ClassHerancaEncapsulamentoSobrecarga/classeProduto/models/Produto.js
@@ -0,0 +1,23 @@
+class Produto {
+  constructor(nome, preco, quantidade) {
+    this.nome = nome;
+    this.preco = preco;
+    this.quantidade = quantidade;
+  }
+  calcularTotal() {
+    return this.preco * this.quantidade;
+  }
+  exibirInfo() {
+    return `${this.nome} - Preço: R$${this.preco.toFixed(2)} - Quantidade: ${
+      this.quantidade
+    } -Total: R$${this.calcularTotal().toFixed(2)}`;
+  }
+  atualizarQuantidade(novaQuantidade) {
+    if (novaQuantidade >= 0) {
+      this.quantidade = novaQuantidade;
+      return true;
+    }
+    return false;
+  }
+}
+export default Produto;
---
+++ Curso/aula20-ClassHerancaEncapsulamentoSobrecarga/classeProduto/script.js
@@ -0,0 +1,90 @@
+import Produto from "./models/Produto.js";
+const listaProdutos = [];
+function adicionarProduto() {
+  const nome = prompt("Digite o nome do produto:");
+  const preco = parseFlot(prompt("Digite o preço do produto:"));
+  const quantidade = parseInt(prompt("Digite a quantidade do produto:"));
+
+  if (nome && !isNaN(preco) && !isNaN(quantidade)) {
+    const produto = new Produto(nome, preco, quantidade);
+    listaProdutos.push(produto);
+    alert("Produto adicionado com sucesso");
+  } else {
+    alert("Por favor, preencha todos os campos corretamente");
+  }
+}
+
+function listarProdutos() {
+  document.open();
+  if (listaProdutos.length === 0) {
+    document.write("Nenhum produto cadastrado<br>");
+    document.close();
+    return;
+  }
+  document.write("Lista de produto:<br>");
+  listaProdutos.forEach((produto, index) => {
+    document.write(`${index + 1}. ${produto.exibirInfo()}`);
+  });
+  document.close();
+}
+
+function editarQuantidade() {
+  const index = parseInt(
+    prompt("Digite o número do produto para editar a quantidade")
+  );
+  if (index >= 0 && index < listaProdutos.length) {
+    const novaQuantidade = parseInt(prompt("Digite a nova quantidade:"));
+
+    if (!isNaN(novaQuantidade)) {
+      listaProdutos[index].atualizarQuantidade(novaQuantidade);
+      alert("Quantidade atualizada com sucesso!");
+    } else {
+      alert("Quantidade inválida.");
+    }
+  } else {
+    alert("Produto não encontrado");
+  }
+}
+
+function deletarProduto() {
+  const index =
+    parseInt(prompt("Digite o número do produto para deletar:")) - 1;
+  if (index >= 0 && index < listaProdutos.length) {
+    const produtoRemovido = listaProdutos.splice(index, 1)[0];
+    alert(`Produto '${produtoRemovido.nome}'deletado com sucesso!`);
+  } else {
+    alert("Produto não encontrado.");
+  }
+}
+
+function menu() {
+  while (true) {
+    const opcao = prompt(
+      "Escolha uma opção:\n1. Adcionar produto\n2. Listar produtos\n3. Editar quantidade\n4. Deletar produto\n5. Sair"
+    );
+
+    switch (opcao) {
+      case "1":
+        adicionarProduto();
+        break;
+      case "2":
+        listarProdutos();
+        break;
+      case "3":
+        editarQuantidade();
+        break;
+      case "4":
+        deletarProduto();
+        break;
+      case "5":
+        alert("Saindo...");
+        break;
+
+      default:
+        alert("Opção Inválida.");
+        break;
+    }
+  }
+}
+
+menu();
